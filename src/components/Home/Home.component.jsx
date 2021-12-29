bb515a7b7ac9489a5f9cdc6f5a83ab462c8d2e87
jeck99 <jeck99@gmail.com>
2021-12-29 14:10:21 +0200

    Switch-Case use to route components on home-page

--- src/components/Home/Home.component.jsx
+++ src/components/Home/Home.component.jsx
@@ -1,15 +1,30 @@
 import React, { useState } from 'react';
-
+import ChildComponent from "../Child/Child.component";
+import TeacherComponent from "../Teacher/Teacher.component";
+import ParentComponent from "../Parent/Parent.component";
 export default function HomeComponent() {
     const [page, setPage] = useState(0);
-    const Routing = () => { };
+    const Routing = () => {
+        switch (page) {
+            case 1:
+                return <ChildComponent />
+            case 2:
+                return <TeacherComponent />
+            case 3:
+                return <ParentComponent />
+            default:
+                return <div></div>;
+        }
+    };
     return (
         <div>
-            <button onClick={()=>setPage(1)}>Child Page</button>
-            <button onClick={()=>setPage(2)}>Teacher Page</button>
-            <button onClick={()=>setPage(3)}>Parent Page</button>
+            <button onClick={() => setPage(1)}>Child Page</button>
+            <button onClick={() => setPage(2)}>Teacher Page</button>
+            <button onClick={() => setPage(3)}>Parent Page</button>
             <hr />
             This is Home Page
+            <hr />
+            <Routing/>
         </div>
     );
 }
